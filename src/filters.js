1eaba9f590ab0b3b707d6c673ba328576dd86b87
Luiz Normanha Marques Pereira <luiznormanha@gmail.com>
2022-06-07 21:11:00 -0300

    feat: wip config for specific collections

--- src/filters.js
+++ src/filters.js
@@ -1,18 +1,20 @@
 // Load modules and constants
 const { combinationOfTraitsAlreadyExists } = require('./filters/combination_traits');
-const { incompatibleTraitsUsed } = require('./filters/incompatible_traits');
+const { incompatibleTraitsUsed, traitHasDefinedIncompatibilities } = require('./filters/incompatible_traits');
 const { dependentTraitsUsed } = require('./filters/dependent_traits');
 
 // Checks the different filtrations and return true if any of them are true
 const needsFiltration = (selectedTraitsList, newTraits, maxRepeatedTraits, incompatibleTraits, layerItemsMaxRepeatedTraits, dependentTraits) => {
 
-  return (
-    combinationOfTraitsAlreadyExists(selectedTraitsList, newTraits, maxRepeatedTraits, layerItemsMaxRepeatedTraits) 
-    || incompatibleTraitsUsed(newTraits, incompatibleTraits)
-    || dependentTraitsUsed(newTraits, dependentTraits)
-  );
+  const willFilter = combinationOfTraitsAlreadyExists(selectedTraitsList, newTraits, maxRepeatedTraits, layerItemsMaxRepeatedTraits)
+      || incompatibleTraitsUsed(newTraits, incompatibleTraits)
+      || dependentTraitsUsed(newTraits, dependentTraits)
+
+  console.log({willFilter})
+  return willFilter
 };
 
 module.exports = {
   needsFiltration,
+  traitHasDefinedIncompatibilities
 };
